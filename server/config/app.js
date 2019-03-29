3ea48a4ec13703248a3fee1aa7eed0b82e5f566c
troyfischer <heat444@ufl.edu>
2019-03-29 12:54:16 -0400

    modified app.js

--- server/config/app.js
+++ server/config/app.js
@@ -4,10 +4,7 @@ var config = require('./config'),
 
 module.exports.start = function() {
     var app = express.init();
-    //app.listen(config.port, function() {
-    //console.log('App listening on port', config.port);
-    //});
-    app.listen(process.env.PORT || 3000, function() {
-        console.log("Express server listening on port %d in %s mode", this.address.port, app.settings.env);
+    app.listen(config.port, function() {
+    console.log('App listening on port', config.port);
     });
 };

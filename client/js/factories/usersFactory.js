3d3cb21dba2bce5a3795f8478b9d24e8ae95774f
troyfischer <troyfischer4@gmail.com>
2019-04-01 20:37:54 -0400

    heroku config

--- client/js/factories/usersFactory.js
+++ client/js/factories/usersFactory.js
@@ -1,10 +1,21 @@
 angular.module('users', []).factory('Users', function($http) {
+  // uncomment if you want to run locally
+  // var methods = {
+  // 	registerUser: function(user) {
+  //       return $http.post('http://localhost:8080/auth/register', user);
+  //   },
+  //   loginUser: function(user) {
+  //       return $http.post('http://localhost:8080/auth/login', user);
+  //   }
+  // };
+
+  // uncomment if you want to run on heroku
   var methods = {
   	registerUser: function(user) {
-        return $http.post('http://localhost:8080/auth/register', user);
+        return $http.post('https://pumpkin-tart-18443.herokuapp.com/auth/register', user);
     },
     loginUser: function(user) {
-        return $http.post('http://localhost:8080/auth/login', user);
+        return $http.post('https://pumpkin-tart-18443.herokuapp.com/auth/login', user);
     }
   };
 

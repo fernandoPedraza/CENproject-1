b99516e8e6213e34533b8314cd1775ab83d99e95
Troy Fischer <troyfischer4@gmail.com>
2019-04-12 18:54:08 -0400

    refactored

--- client/js/app.js
+++ client/js/app.js
@@ -1,7 +1,3 @@
-/* register the modules the application depends upon here*/
-angular.module('listings', []);
-
-
 /* register the application and inject all the necessary dependencies */
 var app = angular.module('directoryApp', ['listings']);
 

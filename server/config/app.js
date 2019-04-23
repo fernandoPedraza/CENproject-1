992af08e5afb9914b4dc96d9419cdc12bb11a641
troyfischer <troyfischer4@gmail.com>
2019-04-22 20:09:47 -0400

    heroku deployment config

--- server/config/app.js
+++ server/config/app.js
@@ -4,12 +4,12 @@ var config = require('./config'),
 module.exports.start = function() {
     var app = express.init();
     // uncomment if you want to run locally
-    app.listen(config.port, function() {
-        console.log('App listening on port', config.port);
-    });
+    // app.listen(config.port, function() {
+    //     console.log('App listening on port', config.port);
+    // });
 
     // uncomment if you want it to work with heroku
-    // app.listen(process.env.PORT || 3000, function() {
-    //     console.log("Express server listening on port %d in %s mode", this.address.port, app.settings.env);
-    // });
+    app.listen(process.env.PORT || 3000, function() {
+        console.log("Express server listening on port %d in %s mode", this.address.port, app.settings.env);
+    });
 };
